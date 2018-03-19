272ae6acf898ebb35b30ebb9685de357f8fbdec5
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-03-19 17:06:15 +0100

    try creating struct on the stack and pass as non pointer parameter

--- src/ex16.c
+++ src/ex16.c
@@ -52,20 +52,33 @@ void Person_print(struct Person *who)
 /**
  *
  */
+void Person_print2(struct Person who)
+{
+  printf("Name: %s\n", who.name);
+  printf("\tAge: %d\n", who.age);
+  printf("\tHeight: %d\n", who.height);
+  printf("\tWeight: %d\n", who.weight);
+}
+
+/**
+ *
+ */
 int main(int argc, char *argv[])
 {
   printf("sizeof (struct Person): %ld\n", sizeof (struct Person));
 
-  // make two people structures
   struct Person *joe = Person_create("Joe Alex", 32, 64, 140);
-  struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
-
-  // print them out and where they are in memory
   printf("Joe is at memory location: %p\n", joe);
   Person_print(joe);
 
+  struct Person *frank = Person_create("Frank Blank", 20, 72, 180);
   printf("Frank is at memory location: %p\n", frank);
   Person_print(frank);
+
+  struct Person stack = { "Mr Stack Struct", 88, 99, 314 };
+  printf("Stack is at memory location: %p\n", &stack);
+  Person_print(&stack);
+  Person_print2(stack);
 
   // make everyone age 20 years and print them again
   joe->age += 20;
