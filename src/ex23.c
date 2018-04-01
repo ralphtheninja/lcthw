2d323f531ee2be97b9a5c0de50735d91507ef95a
Lars-Magnus Skog <ralphtheninja@riseup.net>
2018-04-01 19:23:34 +0200

    define LENGTH as 1000

--- src/ex23.c
+++ src/ex23.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "dbg.h"
 
+#define LENGTH 1000
+
 int zeds_device(char *from, char *to, int count)
 {
   int n = (count + 7) / 8;
@@ -85,40 +87,40 @@ int valid_copy(char *data, int count, char expects)
 
 int main(int argc, char **argv)
 {
-  char from[1000] = { 'a' };
-  char to[1000] = { 'c' };
+  char from[LENGTH] = { 'a' };
+  char to[LENGTH] = { 'c' };
   int rc = 0;
 
   // set up the from to have some stuff
-  memset(from, 'x', 1000);
-  check(valid_copy(from, 1000, 'x'), "from not initialized right");
+  memset(from, 'x', LENGTH);
+  check(valid_copy(from, LENGTH, 'x'), "from not initialized right");
 
   // set it to a failure mode
-  memset(to, 'y', 1000);
-  check(valid_copy(to, 1000, 'y'), "to not initialized right");
+  memset(to, 'y', LENGTH);
+  check(valid_copy(to, LENGTH, 'y'), "to not initialized right");
 
   // use normal copy to
-  rc = normal_copy(from, to, 1000);
-  check(rc == 1000, "Normal copy failed: %d", rc);
-  check(valid_copy(to, 1000, 'x'), "Normal copy failed");
+  rc = normal_copy(from, to, LENGTH);
+  check(rc == LENGTH, "Normal copy failed: %d", rc);
+  check(valid_copy(to, LENGTH, 'x'), "Normal copy failed");
 
   // reset
-  memset(to, 'y', 1000);
-  check(valid_copy(to, 1000, 'y'), "to not initialized right");
+  memset(to, 'y', LENGTH);
+  check(valid_copy(to, LENGTH, 'y'), "to not initialized right");
 
   // duffs version
-  rc = duffs_device(from, to, 1000);
-  check(rc == 1000, "Duff's device failed: %d", rc);
-  check(valid_copy(to, 1000, 'x'), "Duff's device failed copy.");
+  rc = duffs_device(from, to, LENGTH);
+  check(rc == LENGTH, "Duff's device failed: %d", rc);
+  check(valid_copy(to, LENGTH, 'x'), "Duff's device failed copy.");
 
   // reset
-  memset(to, 'y', 1000);
-  check(valid_copy(to, 1000, 'y'), "to not initialized right");
+  memset(to, 'y', LENGTH);
+  check(valid_copy(to, LENGTH, 'y'), "to not initialized right");
 
   // my version
-  rc = zeds_device(from, to, 1000);
-  check(rc == 1000, "Zed's device failed: %d", rc);
-  check(valid_copy(to, 1000, 'x'), "Zed's device failed copy.");
+  rc = zeds_device(from, to, LENGTH);
+  check(rc == LENGTH, "Zed's device failed: %d", rc);
+  check(valid_copy(to, LENGTH, 'x'), "Zed's device failed copy.");
 
   return 0;
 
